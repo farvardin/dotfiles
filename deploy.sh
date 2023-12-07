4f6d78fe7e4ebca794f8810df80da32e8a81b0d3
Eric <farvardin@local>
2023-12-07 09:21:50 +0100

    add geany

--- deploy.sh
+++ deploy.sh
@@ -135,7 +135,7 @@ done
 
 # .config
 
-for R in .config/emacs .config/ghostwriter .config/castor .config/geany 
+for R in .config/emacs .config/ghostwriter .config/castor  .config/geany
 do
     if [ -e ~/$R ]; then
 		echo -e "~/$R is already present on your system."
@@ -146,6 +146,8 @@ do
 done
 
 # .config/GottCode ?
+
+
 
 # .local/share 
 
