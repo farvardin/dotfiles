c7a2c7fc3623f50f8ac55502dd429dce6a8e22fd
Eric <farvardin@local>
2025-10-23 10:51:42 +0200

    adding date in scite

--- SciTEStartup.lua
+++ SciTEStartup.lua
@@ -4,3 +4,9 @@
 -- dofile (props["SciteUserHome"].."/.scite/tictactoe.lua") 
 -- dofile (props["SciteUserHome"].."/.scite/eliza.lua") 
 -- dofile (props["SciteUserHome"].."/.scite/scitefunclist.lua") 
+--dofile (props["SciteUserHome"].."/.scite/date.lua") 
+
+function insertDate() 
+	MyDate = os.date("\n== %Y-%m-%d ==\n")
+    editor:InsertText(-1,MyDate)
+end
